098e4e05dbfd50976e57a8729098f3f1b386fb27
Ed Johnsen <EdJohnsen@users.noreply.github.com>
2021-07-17 15:42:12 -0600

    Update deepCloneES6.js

--- deepClone/deepCloneES6.js
+++ deepClone/deepCloneES6.js
@@ -1,206 +1,222 @@
 /*GNU LGPLv3*/
 
-// GENERAL SUPPORT VARIABLES
-let errorConstructor = {
-	"Error":true,
-	"EvalError":true,
-	"RangeError":true,
-	"ReferenceError":true,
-	"SyntaxError":true,
-	"TypeError":true,
-	"URIError":true
-};
+var deepCloneES6 = (function(){
+	// GENERAL SUPPORT VARIABLES
+	let errorConstructor = {
+		"Error":true,
+		"EvalError":true,
+		"RangeError":true,
+		"ReferenceError":true,
+		"SyntaxError":true,
+		"TypeError":true,
+		"URIError":true
+	};
 
-let filledConstructorES3 = {
-	"Boolean":true,
-	"Date":true,
-	"String":true,
-	"Number":true,
-	"RegExp":true
-};
+	let filledConstructorES3 = {
+		"Boolean":true,
+		"Date":true,
+		"String":true,
+		"Number":true,
+		"RegExp":true
+	};
 
-let filledConstructorES6 = {
-	"BigInt":true,
-	"Symbol":true
-};
+	let filledConstructorES6 = {
+		"BigInt":true,
+		"Symbol":true
+	};
 
-let arrayConstructorsES5 = {
-	"Array":true,
-	"BigInt64Array":true,
-	"BigUint64Array":true,
-	"Float32Array":true,
-	"Float64Array":true,
-	"Int8Array":true,
-	"Int16Array":true,
-	"Int32Array":true,
-	"Uint8Array":true,
-	"Uint8ClampedArray":true,
-	"Uint16Array":true,
-	"Uint32Array":true
-};
+	let arrayConstructorsES5 = {
+		"Array":true,
+		"BigInt64Array":true,
+		"BigUint64Array":true,
+		"Float32Array":true,
+		"Float64Array":true,
+		"Int8Array":true,
+		"Int16Array":true,
+		"Int32Array":true,
+		"Uint8Array":true,
+		"Uint8ClampedArray":true,
+		"Uint16Array":true,
+		"Uint32Array":true
+	};
 
-let arrayConstructorsES6 = {
-	"ArrayBuffer":true,
-	"SharedArrayBuffer":true
-};
-
-
-// DEEP-CLONING FUNCTIONS ES6
-function deepCloneES6(obj, circMap = new WeakMap()){
-
-	if(
-		obj !== null && 
-		typeof obj === "object" &&
-		!circMap.get(obj)
-	){
+	let arrayConstructorsES6 = {
+		"ArrayBuffer":true,
+		"SharedArrayBuffer":true
+	};
 
 
-		circMap.set(obj, true);
+	// DEEP-CLONING FUNCTIONS ES6
+	function deepCloneES6(obj, circMap = new WeakMap()){
+
+		if(
+			obj !== null && 
+			typeof obj === "object" &&
+			!circMap.get(obj)
+		){
 
 
-		let newObj;
+			circMap.set(obj, true);
 
-		if(obj.constructor){
 
-			let oType = obj.constructor.name;
+			let newObj;
 
-			if(oType === "Object"){
+			if(obj.constructor){
 
-				let longName = Object.prototype.toString.call(obj);
+				let oType = obj.constructor.name;
 
-				if(longName === "[object Generator]")
-					return obj;
+				if(oType === "Object"){
 
-				else if(longName === "[object Arguments]")
-					newObj = new Array(obj.length); // not multi-frame complete... and mangles object type
+					let longName = Object.prototype.toString.call(obj);
 
-				else
-					newObj = Object.create(Object.getPrototypeOf(obj)); // not multi-frame complete... but handles malformed constructors
-					// if you are sure that constructors are well formed, use the following:
-					// newObj = new obj.constructor(); // multi-frame complete
-				
-			}
+					if(longName === "[object Generator]")
+						return obj;
 
-			else if(filledConstructorES3[oType])
-				newObj = new obj.constructor(obj);
+					else if(longName === "[object Arguments]")
+						newObj = new Array(obj.length); // not multi-frame complete, and mangles object type
 
-			else if(filledConstructorES6[oType])
-				newObj = obj.constructor(obj);
+					else
+						newObj = new obj.constructor();
 
-			else if(arrayConstructorsES5[oType])
-				newObj = new obj.constructor(obj.length);
-
-			else if(arrayConstructorsES6[oType])
-				newObj = new obj.constructor(obj.byteLength);
-
-			else if(oType === "DataView")
-				newObj = new obj.constructor(obj.buffer);
-
-			else if ( errorConstructor[oType] ){
-
-				if(obj.stack){
-
-					newObj = new obj.constructor(obj.message);
-
-					newObj.stack = obj.stack;
 				}
 
-				else
-					newObj = new Error(obj.message + " INACCURATE OR MISSING STACK-TRACE");
-				
-			}
+				else if(filledConstructorES3[oType])
+					newObj = new obj.constructor(obj);
 
-			else if(oType === "Set"){
+				else if(filledConstructorES6[oType])
+					newObj = obj.constructor(obj);
 
-				newObj = new Set();
+				else if(arrayConstructorsES5[oType])
+					newObj = new obj.constructor(obj.length);
 
-				obj.forEach((value, key)=>{
+				else if(arrayConstructorsES6[oType])
+					newObj = new obj.constructor(obj.byteLength);
 
-					let aVal = value !== null 
-						&& typeof value === "object" 
-						? deepCloneES6( value, circMap)
-						: value;
+				else if(oType === "DataView")
+					newObj = new obj.constructor(obj.buffer);
 
-					newObj.set( key, aVal );
-				});			
-			}
+				else if ( errorConstructor[oType] ){
 
-			else if(oType === "Map"){
+					if(obj.stack){
 
-				newObj = new Map();
+						newObj = new obj.constructor(obj.message);
 
-				obj.forEach((value, key)=>{
+						newObj.stack = obj.stack;
+					}
 
-					let aKey = typeof key === "object" 
-						&& key !== null 
-						? deepCloneES6( key, circMap) 
-						: key;
+					else
+						newObj = new Error(obj.message + " INACCURATE OR MISSING STACK-TRACE");
 
-					let aVal = typeof value === "object" 
-						&& value !== null 
-						? deepCloneES6( value, circMap) 
-						: value;
+				}
 
-					newObj.set( aKey, aVal );
-				});
+				else if(oType === "Set"){
+
+					newObj = new Set();
+
+					obj.forEach((value, key)=>{
+
+						let aVal = value !== null 
+							&& typeof value === "object" 
+							? deepCloneES6( value, circMap)
+							: value;
+
+						newObj.set( key, aVal );
+					});			
+				}
+
+				else if(oType === "Map"){
+
+					newObj = new Map();
+
+					obj.forEach((value, key)=>{
+
+						let aKey = typeof key === "object" 
+							&& key !== null 
+							? deepCloneES6( key, circMap) 
+							: key;
+
+						let aVal = typeof value === "object" 
+							&& value !== null 
+							? deepCloneES6( value, circMap) 
+							: value;
+
+						newObj.set( aKey, aVal );
+					});
+				}
+
+				else{
+
+					newObj = new obj.constructor();
+
+					let cProps = Object.getOwnPropertyNames(newObj);
+
+					let cI = 0,
+					    cImax = cProps.length;
+
+					for(;cI<cImax; cI++)
+						if( !obj.hasOwnProperty( cProps[cI] ) )
+							delete newObj[ cProps[cI] ];
+
+				}
+
 			}
 
 			else
-				return obj;
-
-		}
-
-		else
-			newObj = Object.create(null); 
+				newObj = Object.create(null); 
 
 
-		let props = Object.getOwnPropertyNames(obj);
+			let props = Object.getOwnPropertyNames(obj),
+			    prop,
+			    descriptor;
 
-		let descriptor;
+			for(let i in props){
 
-		for(let i in props){
+				descriptor = Object.getOwnPropertyDescriptor( obj, props[i] );
 
-			descriptor = Object.getOwnPropertyDescriptor( obj, props[i] );
+				prop = props[i];
 
-			prop = props[i];
+				if(
+					descriptor.value &&
+					typeof descriptor.value === "object" &&
+					descriptor.value !== null && 
+					!circMap.get( descriptor.value )
 
-			if(descriptor.value){
+				){
 
-				if(descriptor.value !== null && 
-					typeof descriptor.value === "object"
-				)
+					Object.defineProperty( newObj, prop, descriptor );
+
 					newObj[prop] = deepCloneES6( obj[prop], circMap);
+				}
+
+				else if(descriptor.get || descriptor.set) 
+					continue;
 
 				else
-					newObj[prop] = obj[prop];
+					Object.defineProperty( newObj, prop, descriptor );
 
 			}
 
-			else if(descriptor.get || descriptor.set) 
-				continue;
 
-			else
-				Object.defineProperty( newObj, prop, descriptor );
+			let syms = Object.getOwnPropertySymbols(obj);
 
+			for(let i = 0, imax = syms.length; i<imax; i++){
+
+				if(obj[syms[i]] !== null && 
+					typeof obj[syms[i]] === "object"
+				)
+					newObj[syms[i]] = cloneItES6( syms[i], circMap);
+
+				else
+					newObj[syms[i]] = obj[syms[i]];
+
+			}
+
+			return newObj;
 		}
 
-
-		let syms = Object.getOwnPropertySymbols(obj);
-
-		for(let i = 0, imax = syms.length; i<imax; i++){
-
-			if(obj[syms[i]] !== null && 
-				typeof obj[syms[i]] === "object"
-			)
-				newObj[syms[i]] = cloneItES6( syms[i], circMap);
-
-			else
-				newObj[syms[i]] = obj[syms[i]];
-
-		}
-
-		return newObj;
+		return obj;
 	}
-
-	return obj;
-}
+	
+	
+	return deepCloneES6;
+})();
